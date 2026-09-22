f35791734426e4906e83df512fc12584a6eebf01
Rolando Valle <rolandovalle0428@gmail.com>
2026-09-22 23:04:51 +0800

    update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 import { Helmet } from "react-helmet-async";
 import { siteConfig } from "@/content/site";
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://christian-and-geneva-save-the-date.vercel.app/";
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://sunshyne-and-brandon-save-the-date.vercel.app/";
 const canonicalUrl = siteUrl.replace(/\/$/, "");
 
 const desktopHero = "/LinkPreview.png";
